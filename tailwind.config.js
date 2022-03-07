f3f1f3fc6941b8b510f1b99efb83838affafa3ea
Gabriele Nunez <gabrielenunez@protonmail.com>
2022-03-07 07:24:34 -0500

    Added correct version of atrament and used font-headline custom css class

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     extend: {
       fontFamily: {
         'sans': ['acumin-pro','sans-serif'],
-        'headline': ['atrament','sans-serif']
+        'headline': ['atrament-web','sans-serif']
       }
     },
   },
