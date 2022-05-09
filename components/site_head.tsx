30749aa838af5e9c497f532c11e2630367ca79d1
Austin Harms <austin@harmsonline.com>
2022-05-09 09:57:10 -0500

    Move head links to _document

--- components/site_head.tsx
+++ components/site_head.tsx
@@ -24,19 +24,6 @@ export class SiteHead extends React.Component<
           name="viewport"
           content="width=device-width,initial-scale=1,shrink-to-fit=no"
         />
-        {/* Favicon Support */}
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        {/* adobe typekit support */}
-        <link rel="stylesheet" href="https://use.typekit.net/pfr8gmr.css" />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
-        />
 
         {this.props.children}
       </Head>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,27 @@
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentInitialProps,
-  DocumentContext,
-} from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 
-export default class MyDocument extends Document {
-  public render() {
-    return (
-      <Html lang="en">
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
+export const Document = () => (
+  <Html lang="en">
+    <Head>
+      {/* Favicon Support */}
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      {/* adobe typekit support */}
+      <link rel="stylesheet" href="https://use.typekit.net/pfr8gmr.css" />
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+      />
+    </Head>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default Document;
