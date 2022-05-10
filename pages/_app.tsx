34d2e550e700c74efef1939c4ee2135b685e1158
Gabriele Nunez <gabrielenunez@protonmail.com>
2022-05-09 23:56:13 -0400

    Moved meta tags from _document to _app. NextJs was complaining

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,12 +7,26 @@ import type { AppProps } from "next/app";
 
 import smoothScroll from "smoothscroll-polyfill";
 import { useEffect } from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     smoothScroll.polyfill();
   });
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* added meta tags */}
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta charSet="UTF-8" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,shrink-to-fit=no"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
