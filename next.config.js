5bbc7906d21727e8763666071b873a6709560f77
Gabriele Nunez <gabrielenunez@protonmail.com>
2022-05-24 11:45:20 -0400

    Removed bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-  reactStrictMode: true,
+module.exports = {
+   reactStrictMode: true,
   images: {
     domains: ['assets.levelcrush.com']
   },
   optimizeFonts: false,
-});
-
-module.exports = withBundleAnalyzer;
+}
