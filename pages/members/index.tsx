0e453bdf7c9f9b14b96dff376a9a24000e1059b2
Gabriele Nunez <g_m_nunez@live.com>
2022-07-27 11:20:08 -0400

    Restored member pages for testing. For Login with discord updated login button with proper header to work with new login.levelcrush changes

---
+++ pages/members/index.tsx
@@ -0,0 +1,17 @@
+import LoginGuard from "../../components/login_guard";
+import OffCanvas from "../../components/offcanvas";
+import SiteHeader from "../../components/site_header";
+import UnderConstruction from "../../components/under_construction";
+
+export const MemberHomePage = (props: any) => (
+  <OffCanvas>
+    <SiteHeader />
+    <main>
+      <LoginGuard>
+        <UnderConstruction />
+      </LoginGuard>
+    </main>
+  </OffCanvas>
+);
+
+export default MemberHomePage;
