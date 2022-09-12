073331420789056fb5f872c426d1626feba0e7f4
Gabriele Nunez <g_m_nunez@live.com>
2022-09-12 18:09:28 -0400

    Updated more styles related to leaderboards

--- components/speedrun_table.tsx
+++ components/speedrun_table.tsx
@@ -29,10 +29,15 @@ function render_field_value(value: string) {
 
 export const SpeedRunTable = (props: SpeedRunProps) => (
   <div className="container mx-auto speedrun-table" id={props.id}>
-    <table className="block lg:table  table-auto w-full border-black dark:border-white border-[1px] border-solid ">
+    <table className="block lg:table  table-auto w-full border-black dark:border-white lg:border-[1px] border-solid ">
       <thead className="hidden lg:table-header-group">
         <tr className="block lg:table-row border-b-[1px] dark:border-b-white border-b-black">
-          <td className="block lg:table-cell text-left py-1 pl-2">Duration</td>
+          <td className="text-center border-r-none lg:border-r-[1px] border-r-black dark:border-r-white  pl-2 block lg:table-cell lg:text-left ">
+            #
+          </td>
+          <td className="block lg:table-cell text-left py-1 lg:pl-2">
+            Duration
+          </td>
           <td className="block lg:table-cell text-center py-1">Video</td>
           {props.addlHeaders.map((header, header_index) => (
             <td
@@ -48,10 +53,13 @@ export const SpeedRunTable = (props: SpeedRunProps) => (
       <tbody className="block lg:table-row-group">
         {props.data.map((speedrun, speedrun_index) => (
           <tr
-            className="block lg:table-row"
+            className="block border-[1px] border-black dark:border-white lg:table-row my-4 border-t-[1px] border-t-black dark:border-t-white"
             key={"speedrun_" + props.id + "_row_" + speedrun_index}
           >
-            <td className="block lg:table-cell border-b-[1px] border-b-black dark:border-b-white lg:bg-transparent lg:text-left text-center   py-2 lg:py-1 lg:pl-2">
+            <td className="text-center  pl-2 block lg:table-cell lg:text-left py-2 border-r-none lg:border-r-[1px] border-r-black dark:border-r-white border-b-[1px] border-b-black dark:border-b-white lg:bg-transparent ">
+              # {(speedrun_index + 1).toString().padStart(2, "0")}
+            </td>
+            <td className="block lg:table-cell  lg:text-left text-center  lg:pl-2 py-1">
               {speedrun.duration}
             </td>
             <td className="block lg:table-cell text-center py-1">
