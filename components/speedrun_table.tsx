4ee6075fded64b1f12a0e2921276768bb0eccd28
Gabriele Nunez <g_m_nunez@live.com>
2022-09-12 18:38:01 -0400

    Zebra striping

--- components/speedrun_table.tsx
+++ components/speedrun_table.tsx
@@ -31,7 +31,7 @@ export const SpeedRunTable = (props: SpeedRunProps) => (
   <div className="container mx-auto speedrun-table" id={props.id}>
     <table className="block lg:table  table-auto w-full border-black dark:border-white lg:border-[1px] border-solid ">
       <thead className="hidden lg:table-header-group">
-        <tr className="block lg:table-row border-b-[1px] dark:border-b-white border-b-black">
+        <tr className="block bg-white even:bg-slate-100 dark:bg-black dark:even:bg-slate-900 lg:table-row border-b-[1px] dark:border-b-white border-b-black">
           <td className="text-center border-r-none lg:border-r-[1px] border-r-black dark:border-r-white  pl-2 block lg:table-cell lg:text-left ">
             #
           </td>
@@ -53,7 +53,7 @@ export const SpeedRunTable = (props: SpeedRunProps) => (
       <tbody className="block lg:table-row-group">
         {props.data.map((speedrun, speedrun_index) => (
           <tr
-            className="block border-[1px] border-black dark:border-white lg:table-row my-4 border-t-[1px] border-t-black dark:border-t-white"
+            className="bg-white even:bg-slate-100 dark:bg-black dark:even:bg-slate-900 block border-[1px] border-black dark:border-white lg:table-row my-4 border-t-[1px] border-t-black dark:border-t-white"
             key={"speedrun_" + props.id + "_row_" + speedrun_index}
           >
             <td className="text-center  pl-2 block lg:table-cell lg:text-left py-2 border-r-none lg:border-r-[1px] border-r-black dark:border-r-white border-b-[1px] border-b-black dark:border-b-white lg:bg-transparent ">
